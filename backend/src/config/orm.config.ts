d8c9870e39195b6f12a554db2f7d032ed9b78656
Juan Delgado <juandelgadog98@gmail.com>
2023-03-30 21:15:41 -0500

    :recycle: refactor(orm): added orm config

---
+++ backend/src/config/orm.config.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+import { env } from 'process';
+
+export const dataSource: DataSource = new DataSource({
+    type: 'mysql',
+    host: env.DB_HOST,
+    port: parseInt(env.DB_PORT),
+    username: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    entities: [
+        `${__dirname}/../auth/**/*.entity{.ts,.js}`,
+    ],
+    migrations: [`${__dirname}/../migrations/**/*{.ts,.js}`],
+    synchronize: false,
+    migrationsRun: false,
+    logging: false,
+    migrationsTableName: 'migrations',
+    metadataTableName: 'orm_metadata'
+});
