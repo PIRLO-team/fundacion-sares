4d66c64392503791276cad3d10bb2fddb7becd11
Kevin Collazos <kevcollazos@gmail.com>
2023-04-18 13:28:59 -0500

    Fix: Change api route

--- frontend/src/api/projectApi.ts
+++ frontend/src/api/projectApi.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const projectApi = axios.create({
-  baseURL: 'http://fundacion-sares-dev-51ab.up.railway.app/',
+  baseURL: 'https://fundacion-sares-dev-51ab.up.railway.app/',
 });
 
 projectApi.interceptors.request.use((config) => {
