1f5b143a7d07bc4c1a860289092ef90e6de7bd7c
Juan Delgado <juandelgadog98@gmail.com>
2023-05-14 22:05:30 -0500

    :recycle: refactor(Validations): Added class-validator for some DTOs.

--- backend/src/api/direct-volunteer/dto/update-direct-volunteer.dto.ts
+++ backend/src/api/direct-volunteer/dto/update-direct-volunteer.dto.ts
@@ -1,7 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDirectVolunteerDto } from './create-direct-volunteer.dto';
+import { Length } from 'class-validator';
 
 export class UpdateDirectVolunteerDto extends PartialType(CreateDirectVolunteerDto) {
     public is_active: boolean;
+
+    @Length(1, 10000, { message: 'La observación debe contener máximo 10000 caracteres' })
     public observation: string;
 }
